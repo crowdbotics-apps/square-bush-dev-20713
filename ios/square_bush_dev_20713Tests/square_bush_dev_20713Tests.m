d2bb4214efa936d949fc6771aa20a3296694fbfe
Crowdbotics <team@crowdbotics.com>
2021-03-18 20:37:35 +0000

    Initial commit.

---
+++ ios/square_bush_dev_20713Tests/square_bush_dev_20713Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface square_bush_dev_20713 : XCTestCase
+
+@end
+
+@implementation square_bush_dev_20713
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
